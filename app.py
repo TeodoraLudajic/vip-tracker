57793dda9145d573cfeb37b1b0dcb73d66b2581c
TeodoraLudajic <teodoraludajic2@gmail.com>
2026-07-08 14:55:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import streamlit as st
 import pandas as pd
 import sqlite3
 
-# Baza sada čuva i 'brand'
+# Povezivanje sa bazom
 conn = sqlite3.connect('vip_podaci.db')
 c = conn.cursor()
+
+# OVO JE BITNO: Obezbeđujemo da tabela uvek bude ispravna
 c.execute('''CREATE TABLE IF NOT EXISTS baza_igraca 
              (uid TEXT, mesec TEXT, iznos REAL, brand TEXT, closure TEXT, zadovoljan TEXT, voli TEXT, napomena TEXT)''')
 conn.commit()
@@ -19,11 +21,15 @@ if uploaded_file:
     
     if st.button("Učitaj i ažuriraj bazu") and mesec_input:
         for index, row in df.iterrows():
-            # Upisuje UID, mesec, iznos I BRAND
+            # Proveravamo da li kolone postoje u tvom CSV-u
+            uid = str(row['customer_id'])
+            iznos = row['bins']
+            brand = row['brand']
+            
             c.execute("INSERT INTO baza_igraca (uid, mesec, iznos, brand) VALUES (?,?,?,?)", 
-                      (str(row['customer_id']), mesec_input, row['bins'], row['brand']))
+                      (uid, mesec_input, iznos, brand))
         conn.commit()
-        st.success("Podaci su ubačeni u bazu!")
+        st.success("Podaci su uspešno ubačeni u bazu!")
 
 st.divider()
 
@@ -39,7 +45,8 @@ if uid_search:
         vol = st.text_input("Voli (Bonus/FS)?")
         nap = st.text_area("Napomena")
         if st.form_submit_button("Sačuvaj beleške"):
+            # Update beleški za igrača
             c.execute("UPDATE baza_igraca SET closure=?, zadovoljan=?, voli=?, napomena=? WHERE uid=?", 
                       (c_status, zad, vol, nap, uid_search))
             conn.commit()
-            st.success("Sačuvano!")
+            st.success("Beleške sačuvane!")
