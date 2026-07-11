d9d38215b6adc456460b6e06b68be207ff47ea7a
Teodora Ludajic <teodoraludajic2@gmail.com>
2026-07-11 22:43:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -293,8 +293,8 @@ if menu == "📥 Upload Month":
 
     st.subheader("📥 Upload Monthly Report")
 
-    col1, col2, col3 = st.columns([1, 2, 1]) 
-    with col2: 
+    col1, _ = st.columns([1, 2]) 
+    with col1: 
         month = st.text_input("Mesec", placeholder="June 2026")
         file = st.file_uploader("CSV fajl", type="csv")
 
